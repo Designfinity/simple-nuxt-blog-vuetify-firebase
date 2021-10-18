a2d0cb696826547d88f8720ff0cc34724e832d98
Senthilkumar <sskumar.muthupet@gmail.com>
2021-10-18 16:22:55 +0530

    Updated about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
+    <div class="about-page">
         <h1 class="display-3">about page</h1>
     </div>
 </template>
+
+<style>
+.about-page {
+    padding: 10px;
+}
+</style>
