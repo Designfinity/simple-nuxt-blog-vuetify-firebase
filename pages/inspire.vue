0a05900655fd2ee4fdaa9628b9f3b1840832ec6b
Senthilkumar <sskumar.muthupet@gmail.com>
2021-10-18 16:41:55 +0530

    Inspire changes

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,12 +1,12 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5">
+      <img src="/v.png" alt="Vuetify.js" class="mb-6">
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;SSK</em>
           </small>
         </footer>
       </blockquote>
